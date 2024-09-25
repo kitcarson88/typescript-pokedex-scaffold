b170889cde171b5cf488f27589dd67201218a661
Simone Colazzo <simone_colazzo>
2024-09-25 15:07:57 +0200

    cache scaffold

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,7 @@
 export const configuration = {
   SERVICE_TIMEOUT: 5, // Service timeout in seconds
   CACHE_TIMEOUT: 10, // Cache timeout in minutes
+  CACHE_CLEAN_TIMEOUT: 15, // Cache timeout in minutes
   LOGGER_DATE_FORMAT: "DD-MM-YYYY HH:mm:ss Z",
 };
 
--- src/services/cache.ts
+++ src/services/cache.ts
@@ -2,4 +2,4 @@ import { configuration } from "../constants";
 
 const NodeCache = require('node-cache');
 
-export const cache = new NodeCache({ stdTTL: configuration.CACHE_TIMEOUT * 60 });
+export const cache = new NodeCache({ stdTTL: configuration.CACHE_TIMEOUT * 60, checkperiod: configuration.CACHE_CLEAN_TIMEOUT * 60 });
